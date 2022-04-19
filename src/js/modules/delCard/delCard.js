87b9bee805660934584cd975fabf7d657d4477e4
Volha Frantsevich <volhakarobka@gmail.com>
2022-04-19 23:42:25 +0300

    add: btnDelCard local(waiting for id from valeria)

--- src/js/modules/delCard/delCard.js
+++ src/js/modules/delCard/delCard.js
@@ -1,12 +1,23 @@
 import { todo } from '../addUtils/addTasks';
+import { updateLocalStorageForTasks } from "../localStorage/updateLocalStorageTasks";
 
 export function delCard() {
 
 	const btnDelCard = document.getElementsByClassName('btn btn-delete');
 	for (let i = 0; i < btnDelCard.length; i++) {
 		btnDelCard[i].addEventListener('click', (e) => {
-			let parentCard = e.currentTarget.parentElement.parentElement;		
+			let parentCard = e.currentTarget.parentElement.parentElement;
+			let parentCardId = +parentCard.getAttribute('id');
+			console.log(parentCardId + ' id карточки')
 			parentCard.remove();
+
+			todo.forEach((todoObj, i) => {
+				if (parentCardId === todoObj.id) {
+					todo.splice(i, 1);
+				}
+			})
+			updateLocalStorageForTasks(todo);
+			//render?
 		})
 	}
 }
